1d07b99a1657660ca27992643ac7a68577b898ff
Kantemir Shogenov <kantemir-83@yandex.ru>
2022-06-01 00:05:49 +0300

    Reached lesson 3

--- hello.go
+++ hello.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	fmt.Println("Hello they")
+	const lightSpeed = 299792 // km/s
+	var distance = 56000000 // km
+	fmt.Println(distance/lightSpeed, "seconds")
+	distance = 401000000
+	fmt.Println(distance/lightSpeed, "seconds")
+
+	var random = rand.Int()
+
+	fmt.Println("Random number: ", random)
 }
